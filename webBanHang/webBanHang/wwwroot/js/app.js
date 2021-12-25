ed9e0b5fd784d33bb6a84ad1fd53d39659cc43b8
KunMan <84560045+Quan2210@users.noreply.github.com>
2021-12-25 14:37:17 +0700

    add product and productDetail

---
+++ webBanHang/webBanHang/wwwroot/js/app.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module('myApp', ['ngAnimate']);
+
+
+app.controller('productCtrl', ['$scope', function ($scope) {
+
+    
+    $scope.hiddenView = false;
+   
+    // Quick view
+    $scope.quickView = function () {
+        $scope.hiddenView = true;
+    }
+
+    $scope.closeQuickView = function () {
+        $scope.hiddenView = false;
+    }
+
+}]);
